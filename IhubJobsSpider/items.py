8adb910a2564c24d4c07ab12ec72edebc0202fca
EdwardOkech <okechjobs@gmail.com>
2016-01-07 15:46:54 +0300

    modelled items to hold ihub jobs data

--- IhubJobsSpider/items.py
+++ IhubJobsSpider/items.py
@@ -1,6 +1,12 @@
 import scrapy
 
 
+class IhubjobsspiderItem(scrapy.Item):
+    job_title = scrapy.Field()
+    company = scrapy.Field()
+    location = scrapy.Field()
+    link = scrapy.Field()
+
 
 
 
